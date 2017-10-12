8fa3491969fbe332950d8349c7bc077586b948c4
sushrut kasture <sushrut@hotmail.co.in>
2017-10-12 09:09:21 +0530

    install script

---
+++ uprun.sh
@@ -0,0 +1,9 @@
+sudo apt-get install node
+sudo apt-get install nodejs-legacy
+sudo apt-get install npm
+
+npm config set proxy http://172.30.0.7:3128 
+npm config set https-proxy http://172.30.0.7:3128
+
+npm install
+
